c85a313e07603e3adcf6edac5ffafceca10677d2
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 10:56:26 +0300

    even sort in right order, odd sort in reverse

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@
 module.exports = function towelSort (matrix) {
   let newArr = [];
   for (i = 0; i < matrix.length; i++) {
-    for (j = 0; j < matrix[i].length; j++) {
-      newArr.push(matrix[i][j]);
+    if (i % 2 == 0) {
+      for (j = 0; j < matrix[i].length; j++) {
+        newArr.push(matrix[i][j]);
+      }
+    } else {
+      for (j = matrix[i].lenth - 1; j >= 0; j--) {
+        newArr.push(matrix[i][j]);
+      }
     }
   }
   return newArr;
