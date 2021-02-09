08e6722687c86e00bbf6c2ed6bb81427688e15bb
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 11:00:26 +0300

    final result, added matrix=undefined

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
 
 module.exports = function towelSort (matrix) {
   let newArr = [];
+  if (typeof(matrix) == 'undefined') return [];
   for (i = 0; i < matrix.length; i++) {
     if (i % 2 == 0) {
       for (j = 0; j < matrix[i].length; j++) {
         newArr.push(matrix[i][j]);
       }
     } else {
-      for (j = matrix[i].lenth - 1; j >= 0; j--) {
+      for (j = matrix[i].length - 1; j >= 0; j--) {
         newArr.push(matrix[i][j]);
       }
     }
